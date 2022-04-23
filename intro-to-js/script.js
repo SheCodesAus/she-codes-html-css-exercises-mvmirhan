ff57c603b8b67557822d6e9d6cfe4a4aa1f16161
mvmirhan <mvmirhan@yahoo.com>
2022-04-23 12:30:13 +1000

    Worked on intro to js and partially on keyboard

--- intro-to-js/script.js
+++ intro-to-js/script.js
@@ -1,29 +1,44 @@
 window.onload = function() {
     // start
+let starterDiv = document.getElementById('starter-div')
+let firstHeading = document.createElement('h2')
+firstHeading.innerText = 'This is an H2 created with JS'
+firstHeading.classList.add('js-header')
+starterDiv.appendChild(firstHeading)
 
+let secondHeading = document.createElement('h2')
+secondHeading.innerText = 'This is another H2 created with JS'
+secondHeading.classList.add('js-header')
+starterDiv.appendChild(secondHeading)
+secondHeading.id = 'second-h2'
     // end
 }
 
 
 function showName(){
-    console.log('showName');
+console.log('showName')
     // start
+let name = document.getElementById('name').value
+console.log('this is the name variable: ',name)
+document.getElementById('greeting-text').innerText = 'Hi ' + name;
 
     // end
 }
 
 function changeColour(colour) {
     // start
+document.body.style.backgroundColor = colour
 
     // end
 }
 
 function resetColour() {
-    document.body.style.backgroundColor = '#FCE166';
+document.body.style.backgroundColor = '#FCE166'
 }
 
 function toggleLightbulb() {
     // start
-
+let lightbulb = document.getElementById('light')
+lightbulb.classList.toggle("on")
     // end
 }
--- keyboard/script.js
+++ keyboard/script.js
@@ -1,12 +1,20 @@
 window.onload = function() {
-    let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('')
+        console.log("This is the alphabet")
+        console.log(alphabet)
     // start
-
+let keyboard = document.getElementById('keyboard')
     // end
 
     alphabet.forEach(letter => {
         // start
-
+let keyButton = document.createElement('input')
+keyButton.type = 'button'
+keyButton.value = letter
+keyButton.addEventListener('click', event => {
+    highlightLetter(event.target.id)
+    })
+keyboard.appendChild(keyButton);
         // end
     });
 }
@@ -18,6 +26,6 @@ window.onload = function() {
 
 function highlightLetter(letter){
     // start
-
+console.log(letter)
     // end
 }
